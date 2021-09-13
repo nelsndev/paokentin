15954d57bc1be87b159247fa6d93ea551eb3c6df
Nelson <nelsonhenrique11@gmail.com>
2021-09-13 18:56:52 -0300

    Adicionado os itens cadastro-fornada.html e post-fornada.js

---
+++ front-end/js/post-fornada.js
@@ -0,0 +1,21 @@
+document.addEventListener("click", function(event) {
+  if (event.target && event.target.tagName === "BUTTON") {
+    const xhr = new XMLHttpRequest();
+    const url = "http://localhost:8080/fornada/" + event.target.id;
+    xhr.open("post", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        alert("Fornada cadastrada com sucesso.");
+      } else if (xhr.readyState === 4) {
+        alert("Erro ao cadastrar a fornada.");
+      }
+    };
+    xhr.send(JSON.stringify({})); // Obs ****
+  }
+});
+
+/* Obs.:
+ * O back-end foi escrito de modo que nao e necessario enviar os dados do 
+ * objeto fornada, por isso estou enviando um JSON vazio.
+ */
