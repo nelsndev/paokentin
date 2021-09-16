93be4cff75f40f86ccecfe5ed718147f941ae093
Nelson <nelsonhenrique11@gmail.com>
2021-09-15 22:06:06 -0300

    Adicionados: base.css e cadastro-fornada.css

--- front-end/js/form-submit.js
+++ front-end/js/form-submit.js
@@ -1,12 +1,3 @@
-function formToObjectJSON(form) {
-  const formData = new FormData(form);
-  let object = {};
-  for (let [key, value] of formData) {
-    object[key] = value;
-  }
-  return JSON.stringify(object);
-}
-
 document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault();
   const form = document.querySelector("form");
@@ -17,12 +8,21 @@ document.querySelector("form").addEventListener("submit", function (event) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       alert("Item cadastrado com sucesso!");
-      location.href = form.getAttribute("data-redirect");
+      location.reload();
     } else if (xhr.readyState === 4) {
       alert("Erro ao cadastrar o item.");
-      location.href = form.getAttribute("data-redirect");
+      location.reload();
     }
   };
   const objectJSON = formToObjectJSON(form);
   xhr.send(objectJSON);
 });
+
+function formToObjectJSON(form) {
+  const formData = new FormData(form);
+  let object = {};
+  for (let [key, value] of formData) {
+    object[key] = value;
+  }
+  return JSON.stringify(object);
+}
