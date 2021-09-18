840323d4098913ba7cfeafe576b70052e8d7e42b
Nelson <nelsonhenrique11@gmail.com>
2021-09-18 00:20:35 -0300

    countdown-timer.js: modifica let para const

--- front-end/js/countdown-timer.js
+++ front-end/js/countdown-timer.js
@@ -1,22 +1,22 @@
 function addCountdownTimer() {
-  let listSpan = document.getElementsByClassName("countdownTimer");
+  const listSpan = document.getElementsByClassName("countdownTimer");
   for (let index = 0; index < listSpan.length; index++) {
-    let tempoFinal = Number(listSpan[index].getAttribute("data-tempoFim"));
-    let tempoAtual = new Date().getTime();
-    let tempoRestante = tempoFinal - tempoAtual;
+    const tempoFinal = Number(listSpan[index].getAttribute("data-tempoFim"));
+    const tempoAtual = new Date().getTime();
+    const tempoRestante = tempoFinal - tempoAtual;
 
-    let dias = Math.floor(tempoRestante / (1000 * 60 * 60 * 24));
-    let horas = Math.floor(tempoRestante / (1000 * 60 * 60));
-    let minutos = Math.floor(tempoRestante / (1000 * 60));
-    let segundos = Math.floor(tempoRestante / 1000);
+    const dias = Math.floor(tempoRestante / (1000 * 60 * 60 * 24));
+    const horas = Math.floor(tempoRestante / (1000 * 60 * 60));
+    const minutos = Math.floor(tempoRestante / (1000 * 60));
+    const segundos = Math.floor(tempoRestante / 1000);
 
-    let d = dias;
-    let h = horas - dias * 24;
-    let m = minutos - horas * 60;
-    let s = segundos - minutos * 60;
+    const d = dias;
+    const h = horas - dias * 24;
+    const m = minutos - horas * 60;
+    const s = segundos - minutos * 60;
 
-    let minString = (m >= 1) ? `${m}` : `0${m}`;
-    let secString = (s >= 10) ? `${s}` : `0${s}`;
+    const minString = (m >= 1) ? `${m}` : `0${m}`;
+    const secString = (s >= 10) ? `${s}` : `0${s}`;
     listSpan[index].textContent = `${minString}:${secString}`;
 
     if (tempoRestante < 0) {
